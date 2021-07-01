00600334be709b881560f8fdb75e03ae39a37910
kevinkjf02 <kevinkjf02@gmail.com>
2021-06-30 22:51:16 -0500

    Agregado

--- Proyecto Integrador 1.0/Proyecto Integrador/Almacen.cs
+++ Proyecto Integrador 1.0/Proyecto Integrador/Almacen.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string Nombre { get; set; }
         public string RucAlemacen { get; set; }
+        public string Nombresas{ get; set; }
 
     }
 }
