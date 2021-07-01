ad2c0014cf2e32b62d14b3b7154ff3b20aa5b886
kevinkjf02 <kevinkjf02@gmail.com>
2021-06-30 23:14:30 -0500

    sdsd

--- Proyecto Integrador 1.0/Proyecto Integrador/Producto.cs
+++ Proyecto Integrador 1.0/Proyecto Integrador/Producto.cs
@@ -6,7 +6,7 @@ namespace Proyecto_Integrador
     class Producto
     {
         public int CodigoProducto { get; set; }
-        public string Nombre { get; set; }
+        public string Nombres { get; set; }
         public double ValorNutricional { get; set; }
         public float Precio { get; set; }
         public float Precios { get; set; }
